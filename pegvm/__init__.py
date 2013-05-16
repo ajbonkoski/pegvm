3967fd664938405bc62ab4145433383c8b789709
Anthony J Bonkoski <ajbonkoski@gmail.com>
2013-05-16 04:00:48 -0400

    fixed several things to get setup.py working

---
+++ pegvm/__init__.py
@@ -0,0 +1,2 @@
+import peg
+from gen_peg_grammar import PEG_GRAMMAR
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pegvm',
+      version='0.1',
+      description='A PEG-parser engine (VM) that can support dynamic run-time-generated grammars',
+      url='https://bitbucket.org/abonkosk/pegvm',
+      author='Anthony J Bonkoski',
+      author_email='ajbonkoski@gmail.com',
+      packages=['pegvm'],
+      scripts=["bin/pegvm_parse"],
+      zip_safe=False)
