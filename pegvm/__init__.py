d4611b3413f63e057afe6ac3e0d67e28b8629699
Anthony J Bonkoski <ajbonkoski@gmail.com>
2013-09-10 22:40:33 -0400

    added package documentation

--- pegvm/__init__.py
+++ pegvm/__init__.py
@@ -1,3 +1,12 @@
 import peg
 from gen_peg_grammar import PEG_GRAMMAR
 from parse import *
+
+__author__ = "Anthony J Bonkoski"
+__copyright__ = "Copyright 2013, The PegVM Project"
+__credits__ = ["Anthony J Bonkoski"]
+
+__version__ = "1.0.0"
+__maintainer__ = "Anthony J Bonkoski"
+__email__ = "ajbonkoski@jabsoft.io"
+__status__ = "Production"
